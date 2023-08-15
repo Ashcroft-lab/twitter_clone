c1febcf98e4b4bd5ae8eb623094ffd4f72a6eb13
Ashcroft-lab <iadarshgupta@gmail.com>
2023-08-16 00:30:01 +0530

    created user Profiles

---
+++ social/musker/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+# from django.contrib.auth.Models import Groups
+from django.contrib.auth.models import Group, User
+from .models import Profile
+
+# Register your models here.
+admin.site.unregister(Group)
+
+# mix profile info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+# unregister init user
+admin.site.unregister(User)
+# admin.site.register(User)
+admin.site.register(User, UserAdmin)
+# admin.site.register(Profile)
