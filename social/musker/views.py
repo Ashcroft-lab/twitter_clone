274dfdce7643b8ea7bdd196b145517740d6556ab
Ashcroft-lab <iadarshgupta@gmail.com>
2023-08-24 12:56:04 +0530

    Added follow/unfollow Button

--- social/musker/views.py
+++ social/musker/views.py
@@ -11,6 +11,8 @@ def home(request):
 def profile_list(request):
     if request.user.is_authenticated:
         profiles = Profile.objects.exclude(user=request.user)
+
+
         return render(request, "profile_list.html", {"profiles":profiles})
     else:
         messages.success(request, ("you must be logged in to view this page"))
@@ -18,6 +20,30 @@ def profile_list(request):
 def profile(request, pk):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user_id=pk)
+
+
+        #  Post Form Logic
+        if request.method == "POST":
+            # Get curr user id
+            current_user_profile = request.user.profile
+
+            #  get form data
+            action = request.POST['follow']
+
+            # Decide to follow/Unfollow
+
+            if action == "unfollow":
+                current_user_profile.follows.remove(profile)
+
+            elif action == "follow":
+                current_user_profile.follows.add(profile)
+
+            # Save profile
+            current_user_profile.save()
+
+
+
+
         return render(request, "profile.html", {"profile" : profile})
     else:
         messages.success(request, ("you must be logged in to view this page"))
