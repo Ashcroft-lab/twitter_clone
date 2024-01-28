007100aa756a2236abaf1e367247453c3039a5d0
Ashcroft-lab <iadarshgupta@gmail.com>
2024-01-29 00:54:34 +0530

    Likes feature added

--- social/musker/models.py
+++ social/musker/models.py
@@ -11,6 +11,14 @@ class Meep(models.Model):
         )
     body = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(User, related_name='meep_like', blank=True)
+
+
+    # Keep count of likes
+    def number_of_likes(self):
+        return self.likes.count()
+
+
 
     def __str__(self):
         return(
